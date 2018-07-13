644a1ac15c856f679f9c0b112b554861e56b38ef
00010100 <0001101004@protonmail.com>
2018-07-13 14:01:44 +0300

    push notifications

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -9,8 +9,8 @@ module.exports = {
   facebook: {
     clientID: '1679771432332193',
     clientSecret: 'ac577f059f1569bd436fa3de00a52404',
-    callbackURL: hostUrl + 'auth/facebookWeb/callback',
-    callbackUserURL: hostUrl + 'auth/facebookUserWeb/callback',
+    callbackURL: `${hostUrl}auth/facebookWeb/callback`,
+    callbackUserURL: `${hostUrl}auth/facebookUserWeb/callback`,
   },
   mailgun: {
     api_key: 'key-9815e55d5cc3e713ec6bf8777601cb0c',
@@ -27,5 +27,8 @@ module.exports = {
   staticMaxAge: 0,
   google: {
     api_key: 'AIzaSyClRiWMywb9BoMq5bujYVMsceRs5EIqF4c',
+  },
+  notification: {
+    serverKey: 'AAAA81gJKgA:APA91bGEvRXL28V9ClnPW3lbRtHUcKVWjumNT6YgUQfaOvVM8F1_h5bTDtxhlHhvYVBEItbCrm5e3UpRE19ARTw3tdw_BUHyr6AWFSj0DDHqNk6ffwCYXdSYwYMfn7HQpDNtC6mdpPLwiKEVm1X893iLkKFcY2kbOg',
   },
 };
