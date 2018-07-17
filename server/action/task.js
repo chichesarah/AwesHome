24c25f92529bee45e2c225e2af0d6ebd844d3781
00010100 <0001101004@protonmail.com>
2018-07-17 10:25:55 +0300

    get tasks for calendar

--- server/action/task.js
+++ server/action/task.js
@@ -21,45 +21,47 @@ const taskFreeData = [
   'endDate',
 ];
 
+export const convertDataUtc = data => moment(moment(data).format('YYYY-MM-DD') + ' utc');
+
+export const countNextDate = (dueDate, repeat) => {
+  switch (repeat) {
+    case 'day':
+      return moment(dueDate).add(1, 'd');
+    case 'week':
+      return moment(dueDate).add(1, 'w');
+    case '2 weeks':
+      return moment(dueDate).add(2, 'w');
+    case 'month':
+      return moment(dueDate).add(1, 'M');
+    case 'year':
+      return moment(dueDate).add(1, 'y');
+    case 'not repeat':
+      return moment(dueDate);
+    default:
+      return null;
+  }
+};
+
+export const countEndDate = (dueDate, repeat) => {
+  switch (repeat) {
+    case 'day':
+      return moment(dueDate).subtract(1, 'd');
+    case 'week':
+      return moment(dueDate).subtract(1, 'w');
+    case '2 weeks':
+      return moment(dueDate).subtract(2, 'w');
+    case 'month':
+      return moment(dueDate).subtract(1, 'M');
+    case 'year':
+      return moment(dueDate).subtract(1, 'y');
+    case 'not repeat':
+      return moment(dueDate);
+    default:
+      return null;
+  }
+};
+
 class TaskAction {
-  _countNextDate(dueDate, repeat) {
-    switch (repeat) {
-      case 'day':
-        return moment(dueDate).add(1, 'd');
-      case 'week':
-        return moment(dueDate).add(1, 'w');
-      case '2 weeks':
-        return moment(dueDate).add(2, 'w');
-      case 'month':
-        return moment(dueDate).add(1, 'M');
-      case 'year':
-        return moment(dueDate).add(1, 'y');
-      case 'not repeat':
-        return moment(dueDate);
-      default:
-        return null;
-    }
-  }
-
-  _countEndDate(dueDate, repeat) {
-    switch (repeat) {
-      case 'day':
-        return moment(dueDate).subtract(1, 'd');
-      case 'week':
-        return moment(dueDate).subtract(1, 'w');
-      case '2 weeks':
-        return moment(dueDate).subtract(2, 'w');
-      case 'month':
-        return moment(dueDate).subtract(1, 'M');
-      case 'year':
-        return moment(dueDate).subtract(1, 'y');
-      case 'not repeat':
-        return moment(dueDate);
-      default:
-        return null;
-    }
-  }
-
   async create(data) {
     const taskData = _.cloneDeep(data.body);
 
@@ -69,9 +71,8 @@ class TaskAction {
     taskData.taskNameId = data.taskNameObj._id;
     taskData.taskName = data.taskNameObj.name;
 
-    taskData.dueDate = moment(taskData.dueDate);
-
-    taskData.nextDate = this._countNextDate(taskData.dueDate, data.body.repeat);
+    taskData.dueDate = convertDataUtc(taskData.dueDate);
+    taskData.nextDate = taskData.dueDate;
 
     const task = await taskWrite.newTask(taskData);
 
@@ -85,14 +86,19 @@ class TaskAction {
     const taskData = _.assignIn(data.taskObj, data.body);
     delete taskData.taskId;
 
-    taskData.dueDate = moment(taskData.dueDate);
-    let nextDate = this._countNextDate(taskData.dueDate, taskData.repeat);
+    taskData.dueDate = convertDataUtc(taskData.dueDate);
 
-    while (nextDate < today && taskData.repeat !== 'not repeat') {
-      nextDate = this._countNextDate(nextDate, taskData.repeat);
+    if (taskData.dueDate >= moment().startOf('day')) {
+      taskData.nextDate = taskData.dueDate;
+    } else {
+      let nextDate = countNextDate(taskData.dueDate, taskData.repeat);
+
+      while (nextDate < today && taskData.repeat !== 'not repeat') {
+        nextDate = countNextDate(nextDate, taskData.repeat);
+      }
+
+      taskData.nextDate = nextDate;
     }
-
-    taskData.nextDate = nextDate;
 
     const task = await taskWrite.updateTask(taskData);
 
@@ -101,7 +107,7 @@ class TaskAction {
 
   async delete(_id) {
     const taskData = await taskWrite.findById(_id);
-    const endDate = this._countEndDate(taskData.nextDate, taskData.repeat);
+    const endDate = countEndDate(taskData.nextDate, taskData.repeat);
 
     const task = await taskWrite.deleteTask(_id, endDate);
 
@@ -117,7 +123,7 @@ class TaskAction {
       taskData.endDate = taskData.nextDate;
       taskData.isDeleted = true;
     } else {
-      taskData.nextDate = this._countNextDate(taskData.nextDate, taskData.repeat);
+      taskData.nextDate = countNextDate(taskData.nextDate, taskData.repeat);
     }
 
     const task = await taskWrite.completeTask(_id, taskData);
@@ -148,13 +154,19 @@ class TaskAction {
     const tasks = await taskWrite.getOverdueTasks(today);
 
     tasks.forEach((task) => {
-      let nextDate = this._countNextDate(task.nextDate, task.repeat);
+      const currentTask = _.cloneDeep(task);
+      let nextDate = countNextDate(task.nextDate, task.repeat);
 
-      while (nextDate < today && task.repeat !== 'not repeat') {
-        nextDate = this._countNextDate(nextDate, task.repeat);
+      if (task.repeat === 'not repeat') {
+        currentTask.endDate = task.nextDate;
+        currentTask.isDeleted = true;
+      } else {
+        while (nextDate < today) {
+          nextDate = countNextDate(nextDate, task.repeat);
+        }
       }
 
-      taskWrite.updateTask(_.assignIn(task, { nextDate }));
+      taskWrite.updateTask(_.assignIn(currentTask, { nextDate }));
     });
   }
 }
