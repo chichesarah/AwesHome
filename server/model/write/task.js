0c7fac0acce70f836e34390bf15f5ad42a6fb473
00010100 <0001101004@protonmail.com>
2018-08-03 16:52:56 +0300

    change tasks

--- server/model/write/task.js
+++ server/model/write/task.js
@@ -56,24 +56,141 @@ taskWrite.completeTask = (_id, { nextDate, endDate, isDeleted }) =>
   });
 
 taskWrite.getTasksByHousehold = _id =>
-  taskWrite.findRows({
-    query: {
-      householdId: _id,
-      isDeleted: false,
-      $or: notEndedTasks(),
-    },
+  taskWrite.aggregateRows({
+    query: [
+      {
+        $match: {
+          householdId: _id,
+          isDeleted: false,
+          $or: notEndedTasks(),
+        },
+      },
+      {
+        $unwind: '$assignee',
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'assignee',
+          foreignField: '_id',
+          as: 'assignee',
+        },
+      },
+      {
+        $group: {
+          _id: {
+            _id: '$_id',
+            isDeleted: '$isDeleted',
+            householdId: '$householdId',
+            reminder: '$reminder',
+            nextDate: '$nextDate',
+            endDate: '$endDate',
+            taskNameId: '$taskNameId',
+            dueDate: '$dueDate',
+            repeat: '$repeat',
+            ownerId: '$ownerId',
+            taskName: '$taskName',
+            createdAt: '$createdAt',
+            updatedAt: '$updatedAt',
+          },
+          assignee: {
+            $push: '$assignee',
+          },
+        },
+      },
+      {
+        $project: {
+          _id: '$_id._id',
+          isDeleted: '$_id.isDeleted',
+          householdId: '$_id.householdId',
+          reminder: '$_id.reminder',
+          nextDate: '$_id.nextDate',
+          endDate: '$_id.endDate',
+          assignee: '$assignee',
+          taskNameId: '$_id.taskNameId',
+          dueDate: '$_id.dueDate',
+          repeat: '$_id.repeat',
+          ownerId: '$_id.ownerId',
+          taskName: '$_id.taskName',
+          createdAt: '$_id.createdAt',
+          updatedAt: '$_id.updatedAt',
+        },
+      },
+    ],
   });
 
-taskWrite.getByAssignedUser = (_id, householdId) =>
-  taskWrite.findRows({
-    query: {
-      householdId,
-      isDeleted: false,
-      assignee: {
-        $eq: mongoose.Types.ObjectId(_id),
+taskWrite.getByAssignedUser = async (_id, householdId) =>
+  taskWrite.aggregateRows({
+    query: [
+      {
+        $match: {
+          householdId,
+          isDeleted: false,
+          assignee: {
+            $eq: mongoose.Types.ObjectId(_id),
+          },
+          $or: notEndedTasks(),
+        },
       },
-      $or: notEndedTasks(),
-    },
+      {
+        $unwind: '$assignee',
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'assignee',
+          foreignField: '_id',
+          as: 'assignee',
+        },
+      },
+      // {
+      //   $project: {
+      //     assignee: '$assignee',
+      //   },
+      // },
+      {
+        $group: {
+          _id: {
+            isDeleted: '$isDeleted',
+            householdId: '$householdId',
+            reminder: '$reminder',
+            nextDate: '$nextDate',
+            endDate: '$endDate',
+            taskNameId: '$taskNameId',
+            dueDate: '$dueDate',
+            repeat: '$repeat',
+            ownerId: '$ownerId',
+            taskName: '$taskName',
+            createdAt: '$createdAt',
+            updatedAt: '$updatedAt',
+          },
+          assignee: {
+            $push: '$assignee',
+          },
+        },
+      },
+      {
+        $project: {
+          _id: '$_id',
+          isDeleted: '$_id.isDeleted',
+          householdId: '$_id.householdId',
+          reminder: '$_id.reminder',
+          nextDate: '$_id.nextDate',
+          endDate: '$_id.endDate',
+          assignee: '$assignee',
+          taskNameId: '$_id.taskNameId',
+          dueDate: '$_id.dueDate',
+          repeat: '$_id.repeat',
+          ownerId: '$_id.ownerId',
+          taskName: '$_id.taskName',
+          createdAt: '$_id.createdAt',
+          updatedAt: '$_id.updatedAt',
+        },
+      },
+      {
+        $unwind: '$assignee',
+      },
+    ],
   });
 
 taskWrite.getOverdueTasks = today =>
