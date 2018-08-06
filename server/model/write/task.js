de5b6be3a0d019d8711d4f539ed95aab07f3fe3f
00010100 <0001101004@protonmail.com>
2018-08-06 13:41:33 +0300

    owner object added

--- server/model/write/task.js
+++ server/model/write/task.js
@@ -11,6 +11,103 @@ const notEndedTasks = () => [
   { endDate: { $gt: moment().startOf('day').add(1, 'd') } },
 ];
 
+const getTaskList = () => [
+  {
+    $lookup: {
+      from: 'users',
+      localField: 'ownerId',
+      foreignField: '_id',
+      as: 'owner',
+    },
+  },
+  {
+    $unwind: '$owner',
+  },
+  {
+    $unwind: '$assignee',
+  },
+  {
+    $lookup: {
+      from: 'users',
+      localField: 'assignee',
+      foreignField: '_id',
+      as: 'assignee',
+    },
+  },
+  {
+    $unwind: '$assignee',
+  },
+  {
+    $project: {
+      isDeleted: 1,
+      householdId: 1,
+      reminder: 1,
+      nextDate: 1,
+      endDate: 1,
+      taskNameId: 1,
+      dueDate: 1,
+      repeat: 1,
+      taskName: 1,
+      createdAt: 1,
+      updatedAt: 1,
+      'assignee._id': 1,
+      'assignee.firstName': 1,
+      'assignee.lastName': 1,
+      'assignee.avatar': 1,
+      'assignee.createdAt': 1,
+      'assignee.updatedAt': 1,
+      'assignee.isDeleted': 1,
+      'owner._id': 1,
+      'owner.firstName': 1,
+      'owner.lastName': 1,
+      'owner.avatar': 1,
+      'owner.createdAt': 1,
+      'owner.updatedAt': 1,
+      'owner.isDeleted': 1,
+    },
+  },
+  {
+    $group: {
+      _id: {
+        _id: '$_id',
+        isDeleted: '$isDeleted',
+        householdId: '$householdId',
+        reminder: '$reminder',
+        nextDate: '$nextDate',
+        endDate: '$endDate',
+        taskNameId: '$taskNameId',
+        dueDate: '$dueDate',
+        repeat: '$repeat',
+        taskName: '$taskName',
+        createdAt: '$createdAt',
+        updatedAt: '$updatedAt',
+        owner: '$owner',
+      },
+      assignee: {
+        $push: '$assignee',
+      },
+    },
+  },
+  {
+    $project: {
+      _id: '$_id._id',
+      isDeleted: '$_id.isDeleted',
+      owner: '$_id.owner',
+      householdId: '$_id.householdId',
+      reminder: '$_id.reminder',
+      nextDate: '$_id.nextDate',
+      endDate: '$_id.endDate',
+      assignee: '$assignee',
+      taskNameId: '$_id.taskNameId',
+      dueDate: '$_id.dueDate',
+      repeat: '$_id.repeat',
+      taskName: '$_id.taskName',
+      createdAt: '$_id.createdAt',
+      updatedAt: '$_id.updatedAt',
+    },
+  },
+];
+
 taskWrite.newTask = data => taskWrite.insertRow({ data });
 
 taskWrite.updateTask = data =>
@@ -55,94 +152,17 @@ taskWrite.completeTask = (_id, { nextDate, endDate, isDeleted }) =>
     },
   });
 
-taskWrite.getTasksByHousehold = _id =>
+taskWrite.getTasksByHousehold = householdId =>
   taskWrite.aggregateRows({
     query: [
       {
         $match: {
-          householdId: _id,
+          householdId,
           isDeleted: false,
           $or: notEndedTasks(),
         },
       },
-      {
-        $unwind: '$assignee',
-      },
-      {
-        $lookup: {
-          from: 'users',
-          localField: 'assignee',
-          foreignField: '_id',
-          as: 'assignee',
-        },
-      },
-      {
-        $unwind: '$assignee',
-      },
-      {
-        $project: {
-          isDeleted: 1,
-          householdId: 1,
-          reminder: 1,
-          nextDate: 1,
-          endDate: 1,
-          taskNameId: 1,
-          dueDate: 1,
-          repeat: 1,
-          ownerId: 1,
-          taskName: 1,
-          createdAt: 1,
-          updatedAt: 1,
-          'assignee._id': 1,
-          'assignee.firstName': 1,
-          'assignee.lastName': 1,
-          'assignee.avatar': 1,
-          'assignee.avatarId': 1,
-          'assignee.createdAt': 1,
-          'assignee.updatedAt': 1,
-          'assignee.isDeleted': 1,
-        },
-      },
-      {
-        $group: {
-          _id: {
-            _id: '$_id',
-            isDeleted: '$isDeleted',
-            householdId: '$householdId',
-            reminder: '$reminder',
-            nextDate: '$nextDate',
-            endDate: '$endDate',
-            taskNameId: '$taskNameId',
-            dueDate: '$dueDate',
-            repeat: '$repeat',
-            ownerId: '$ownerId',
-            taskName: '$taskName',
-            createdAt: '$createdAt',
-            updatedAt: '$updatedAt',
-          },
-          assignee: {
-            $push: '$assignee',
-          },
-        },
-      },
-      {
-        $project: {
-          _id: '$_id._id',
-          isDeleted: '$_id.isDeleted',
-          householdId: '$_id.householdId',
-          reminder: '$_id.reminder',
-          nextDate: '$_id.nextDate',
-          endDate: '$_id.endDate',
-          assignee: '$assignee',
-          taskNameId: '$_id.taskNameId',
-          dueDate: '$_id.dueDate',
-          repeat: '$_id.repeat',
-          ownerId: '$_id.ownerId',
-          taskName: '$_id.taskName',
-          createdAt: '$_id.createdAt',
-          updatedAt: '$_id.updatedAt',
-        },
-      },
+      ...getTaskList(),
     ],
   });
 
@@ -159,84 +179,7 @@ taskWrite.getByAssignedUser = async (_id, householdId) =>
           $or: notEndedTasks(),
         },
       },
-      {
-        $unwind: '$assignee',
-      },
-      {
-        $lookup: {
-          from: 'users',
-          localField: 'assignee',
-          foreignField: '_id',
-          as: 'assignee',
-        },
-      },
-      {
-        $unwind: '$assignee',
-      },
-      {
-        $project: {
-          isDeleted: 1,
-          householdId: 1,
-          reminder: 1,
-          nextDate: 1,
-          endDate: 1,
-          taskNameId: 1,
-          dueDate: 1,
-          repeat: 1,
-          ownerId: 1,
-          taskName: 1,
-          createdAt: 1,
-          updatedAt: 1,
-          'assignee._id': 1,
-          'assignee.firstName': 1,
-          'assignee.lastName': 1,
-          'assignee.avatar': 1,
-          'assignee.avatarId': 1,
-          'assignee.createdAt': 1,
-          'assignee.updatedAt': 1,
-          'assignee.isDeleted': 1,
-        },
-      },
-      {
-        $group: {
-          _id: {
-            _id: '$_id',
-            isDeleted: '$isDeleted',
-            householdId: '$householdId',
-            reminder: '$reminder',
-            nextDate: '$nextDate',
-            endDate: '$endDate',
-            taskNameId: '$taskNameId',
-            dueDate: '$dueDate',
-            repeat: '$repeat',
-            ownerId: '$ownerId',
-            taskName: '$taskName',
-            createdAt: '$createdAt',
-            updatedAt: '$updatedAt',
-          },
-          assignee: {
-            $push: '$assignee',
-          },
-        },
-      },
-      {
-        $project: {
-          _id: '$_id._id',
-          isDeleted: '$_id.isDeleted',
-          householdId: '$_id.householdId',
-          reminder: '$_id.reminder',
-          nextDate: '$_id.nextDate',
-          endDate: '$_id.endDate',
-          assignee: '$assignee',
-          taskNameId: '$_id.taskNameId',
-          dueDate: '$_id.dueDate',
-          repeat: '$_id.repeat',
-          ownerId: '$_id.ownerId',
-          taskName: '$_id.taskName',
-          createdAt: '$_id.createdAt',
-          updatedAt: '$_id.updatedAt',
-        },
-      },
+      ...getTaskList(),
     ],
   });
 
